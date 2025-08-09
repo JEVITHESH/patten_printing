70e8257e7e0d06b9b73e8512e48138bfae2c436e
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-08-09 22:10:35 +0530

    Create triangle_patten_3.java

---
+++ dynamic/triangle_patten_3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+		int a = sc.nextInt();
+		int count =0;
+		for(int i =1; i <= a ; i++){
+		    for(int j = 1 ; j <= i; j++){
+		        count += 1;
+		        System.out.print(count+" ");
+		    }System.out.print("\n");
+		}
+	}
+} 
+output
+  Enter the number : 5
+  1
+  2 3 
+  4 5 6
+  7 8 9 10
+  11 12 13 14 15 
